41aa695d9eae4d5d0011f13d56d485314c538bfd
Rishabh Tiwari <52770055+rishabh139@users.noreply.github.com>
2026-02-06 11:34:56 +0100

    Change page title from 'The Manifold' to 'Rishabh's Protocol'

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "The Manifold",
+    pageTitle: "Rishabh's Protocol",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
